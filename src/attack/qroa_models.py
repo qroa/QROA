4ebdbdfd4eee390bbe0d9d2cb3e2f765aaaaca1e
sd29206 <hussein.jawad@external.stellantis.com>
2024-08-12 10:48:30 +0200

    test new architecture

--- src/attack/qroa_models.py
+++ src/attack/qroa_models.py
@@ -35,6 +35,7 @@ class SurrogateModel(nn.Module):
         self.fc2 = nn.Linear(128, 32)
         self.fc3 = nn.Linear(32, 1)
 
+        self.fc1 = nn.Linear(self.emb_dim*self.len_coordinates, 128)
 
     def forward(self, x):
 
@@ -43,7 +44,7 @@ class SurrogateModel(nn.Module):
         x = str_emb.transpose(1, 2)
 
         x = F.relu(self.conv1(x))
-        x = torch.flatten(x, start_dim=1)
+        # x = torch.flatten(x, start_dim=1)
         x = self.fc1(x)
         x = F.relu(x)
         x = self.fc2(x)
