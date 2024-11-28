5cf636cdc08e93de7a0d30c08ece745e07d958ee
qroa <qroa405@gmail.com>
2024-11-28 15:23:51 +0100

    Adapted code for use of local model

--- src/global_constants.py
+++ src/global_constants.py
@@ -1,3 +1,6 @@
+import os
+
 PERPLEXITY_MODEL_NAME = 'gpt2'
 PERPLEXITY_THRESHOLD = 1000
-MAX_PARALLELISM_LLM_MODELS = 5
+MAX_PARALLELISM_LLM_MODELS = 100
+PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
