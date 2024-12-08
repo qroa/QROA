7679ec27f6d372bda0f2841ea32d2ff3c274819c
qroa <qroa405@gmail.com>
2024-12-08 23:40:47 +0100

    update

--- src/attack/qroa_models.py
+++ src/attack/qroa_models.py
@@ -135,7 +135,7 @@ class AcquisitionFunction(nn.Module):
                 top_strings = top_strings + self.tokenizer_surrogate_model.batch_decode(top_inputs)
 
             #inputs = self._encode_batch(top_strings)
-            print(top_strings)
+        print(top_strings)
             # predictions = surrogate_model(inputs).T
             # top_indices = torch.topk(predictions, num_samples).indices.view(-1).int()
             # top_inputs = inputs[top_indices, :]
