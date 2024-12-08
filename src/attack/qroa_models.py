3351fff5ddd8b8eee970abee785fe083e76b34d2
qroa <qroa405@gmail.com>
2024-12-08 23:30:33 +0100

    update

--- src/attack/qroa_models.py
+++ src/attack/qroa_models.py
@@ -133,7 +133,7 @@ class AcquisitionFunction(nn.Module):
                 )
 
                 top_inputs = inputs[top_indices, :]
-                top_strings += self.tokenizer_surrogate_model.batch_decode(top_inputs)
+                top_strings = top_strings + self.tokenizer_surrogate_model.batch_decode(top_inputs)
 
         inputs = self._encode_batch(top_strings)
         predictions = surrogate_model(inputs).T
