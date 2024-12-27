c938cb7ceab6eaa603e02f2268afefac1ee22c43
jawadhussein462 <jawadhussein462@gmail.com>
2024-12-27 11:21:20 +0100

    update

--- src/attack/qroa_models.py
+++ src/attack/qroa_models.py
@@ -144,6 +144,7 @@ class AcquisitionFunction(nn.Module):
 
             inputs = []
             batch_size = self.max_dim*2
+            print(len(input_string))
             for s in input_string:
                 str_id = self._encode_string(s)
                 inputs.append(str_id.repeat(batch_size//len(input_string), 1))
