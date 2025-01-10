d97c63d4c2341364bfb46e1d18639e6b764bd6f7
jawadhussein462 <jawadhussein462@gmail.com>
2025-01-10 11:20:19 +0100

    universal suffix

--- src/attack/trigger_generator.py
+++ src/attack/trigger_generator.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Set
+from typing import List, Dict, Set, Tuple
 import random
 from tqdm import tqdm
 import matplotlib.pyplot as plt
@@ -14,7 +14,6 @@ from src.attack.score_function import scoring_function_factory
 from src.attack.qroa_models import SurrogateModel, AcquisitionFunction
 from src.utils import calculate_ucb, calculate_log_prob
 from src.global_constants import PERPLEXITY_MODEL_NAME
-import heapq
 
 
 class TriggerGenerator:
@@ -181,26 +180,12 @@ class TriggerGenerator:
                 self.h[z] = s_z
                 self.n[z] = 1
 
-
-        # Add new triggers to the memory
+        # Ensure the memory does not exceed its maximum capacity by removing the oldest entry
         self.D += triggers
-
-        # Ensure the memory does not exceed its maximum capacity
         while len(self.D) > self.max_d:
-            # Find the least-performing trigger that is not zero
-            least_performing_trigger = min(
-                (trigger for trigger in self.D if self.h.get(trigger, 0) > 0),
-                key=lambda t: self.h[t],
-                default=None  # Fallback if all scores are zero or triggers are missing
-            )
+            self.D.pop(0)
 
-            if least_performing_trigger is not None:
-                self.D.remove(least_performing_trigger)
-            else:
-                # If all remaining triggers have a score of zero, remove the oldest
-                self.D.pop(0)
-
-    def _eval_triggers(self, instruction: str, triggers: List[str]) -> torch.Tensor:
+    def _eval_triggers(self, list_instruction: List[str], triggers: List[str]) -> torch.Tensor:
         """
         Evaluates a list of triggers by appending them to an instruction and observing the language model's response.
 
@@ -209,7 +194,7 @@ class TriggerGenerator:
         that quantifies how well the responses align with the desired outcome (e.g., eliciting harmful content).
 
         Args:
-            instruction (str): The base instruction to which triggers are appended.
+            list_instruction List[str]: The list of instructions to evaluate the triggers.
             triggers (List[str]): A list of trigger strings to be tested.
 
         Returns:
@@ -217,8 +202,8 @@ class TriggerGenerator:
         """
 
         # Create a list of full prompts by appending each trigger to the instruction
-        instructions = [instruction for _ in triggers]  # Replicate the instruction for each trigger
-        prompts = [t + instruction for t in triggers]   # Combine the instruction with each trigger
+        instructions = random.choices(list_instruction, k=len(triggers))
+        prompts = [instructions[i] + t for i, t in enumerate(triggers)]   # Combine the instructions with each trigger
 
         # Use the language model to generate responses for each prompt
         generations = self.model.generate(prompts, max_tokens=self.max_generations_tokens)
@@ -229,25 +214,26 @@ class TriggerGenerator:
         return score_array
 
     def _add_logging(self, 
-                     instruction: str, 
+                     list_instruction: List[str], 
                      trigger: str, 
                      epoch: int) -> None:
 
-        logging_json = {'model_name': self.config["model"],
-                        'embedding_model_name': self.config["embedding_model_path"],
-                        'scoring_method': self.config["scoring_type"],
-                        'nb_tokens': self.coordinates_length,
-                        'batch_size': self.batch_size,
-                        'topk' : self.config["topk"],
-                        'max_d' : self.config["max_d"],
-                        'ucb_c' : self.config['ucb_c'],
-                        'trigger': trigger,
-                        'instruction': instruction,
-                        'average_score': self.h[trigger],
-                        'nb_test': self.n[trigger],
-                        'epoch': epoch,
-                        'budget': self.N,
-                        'loss': self.loss.item()}  
+        for instruction in list_instruction:
+            logging_json = {'model_name': self.config["model"],
+                            'embedding_model_name': self.config["embedding_model_path"],
+                            'scoring_method': self.config["scoring_type"],
+                            'nb_tokens': self.coordinates_length,
+                            'batch_size': self.batch_size,
+                            'topk' : self.config["topk"],
+                            'max_d' : self.config["max_d"],
+                            'ucb_c' : self.config['ucb_c'],
+                            'trigger': trigger,
+                            'instruction': instruction,
+                            'average_score': self.h[trigger],
+                            'nb_test': self.n[trigger],
+                            'epoch': epoch,
+                            'budget': self.N,
+                            'loss': self.loss.item()}  
 
         df_dictionary = pd.DataFrame([logging_json])
         self.logging = pd.concat([self.logging, df_dictionary], ignore_index=True)
@@ -256,12 +242,12 @@ class TriggerGenerator:
         
         return self.logging 
     
-    def _generate_triggers(self, instruction: str) -> List[str]:
+    def _generate_triggers(self, list_instruction: List[str]) -> List[str]:
         """
         Generates and optimizes a list of triggers for a given instruction to maximize the likelihood of inducing specific behavior in a language model.
 
         Args:
-            instruction (str): The malicious instruction for which triggers are generated.
+            list_instruction (List[str]): The list of malicious instructions for which triggers are generated.
 
         Returns:
             List[str]: A list of optimized triggers that have been found to effectively manipulate model responses.
@@ -275,7 +261,7 @@ class TriggerGenerator:
         triggers = self.tokenizer_surrogate_model.batch_decode(trigger_ids)
 
         # Evaluate initial triggers
-        score_array = self._eval_triggers(instruction, triggers)
+        score_array = self._eval_triggers(list_instruction, triggers)
         # Update memory with initial triggers and their scores
         self._update_memory(triggers, score_array)
 
@@ -285,6 +271,7 @@ class TriggerGenerator:
         self.max_n_history = []
 
         # Start optimization process over specified number of epochs
+        # for instruction in list_instruction:
         with tqdm(
             range(self.nb_epochs),
             desc=f"""Best Score: Unkown Loss: Unkown""",
@@ -302,22 +289,21 @@ class TriggerGenerator:
 
                     # Calculate Upper Confidence Bound for each trigger
                     ucb_b =  calculate_ucb(self.h,
-                                           self.n,
-                                           self.N,
-                                           self.ucb_c)
+                                        self.n,
+                                        self.N,
+                                        self.ucb_c)
                     
                     # Select the current best trigger based on UCB
                     trigger = max(self.h, key=lambda key: ucb_b[key])
-                    top_triggers = heapq.nlargest(1, self.h, key=lambda key: ucb_b[key])
 
                     # Select a random token position to modify
                     current_coordinate = self.coordinates[current_epoch % self.coordinates_length]
 
                     # Generate top k new trigger variants by modifying the current trigger at the chosen position
-                    top_k_triggers = self.acquisition_function(self.surrogate_model, top_triggers, current_coordinate, self.topk)
+                    top_k_triggers = self.acquisition_function(self.surrogate_model, trigger, current_coordinate, self.topk)
 
                     # Eval Phase Phase 
-                    score_array = self._eval_triggers(instruction, top_k_triggers)
+                    score_array = self._eval_triggers(list_instruction, top_k_triggers)
                     # Update memory with new triggers and their scores
                     self._update_memory(top_k_triggers, score_array)
                     # Calculate maximum number of times any trigger has been sampled
@@ -330,17 +316,18 @@ class TriggerGenerator:
                     self.best_triggers.add(trigger)
 
                 # Log current epoch results
-                self._add_logging(instruction,
-                                  trigger,
-                                  current_epoch)
+                self._add_logging(list_instruction,
+                                trigger,
+                                current_epoch)
             
                 # Log metrics for the current epoch
                 self.scores_history.append(self.h[trigger])
                 self.losses_history.append(self.loss.cpu().item())
                 self.max_n_history.append(max_n)
 
-                prompt = instruction+trigger
-                progress_bar.set_description(f"Score : {self.h[trigger]}, Loss: {self.loss:.4f}, Prompt: {trigger}, Max n: {max_n}")
+                prompt = list_instruction+[trigger]
+                # progress_bar.set_description(f"Score : {self.h[trigger]}, Loss: {self.loss:.4f}, Max n: {max_n}")
+                progress_bar.set_description(f"Score : {self.h[trigger]}, Prompt : {[prompt]}, Loss: {self.loss:.4f}, Max n: {max_n}")
                 if (self.h[trigger]>self.threshold) and (self.temperature==0):
                         break
                     # resampled_triggers = [trigger]*self.nb_samples
@@ -374,11 +361,10 @@ class TriggerGenerator:
         plt.grid()
         plt.legend()
 
+
         # Loss
         plt.subplot(1, 3, 2)
-        data = self.losses_history
-        data = [sum(data[:i+1])/(i+1) for i in range(len(data))]
-        plt.plot(data, label="Loss", color="red")
+        plt.plot(self.losses_history, label="Loss", color="red")
         plt.title("Loss History")
         plt.xlabel("Epoch")
         plt.ylabel("Loss")
@@ -396,12 +382,13 @@ class TriggerGenerator:
 
         # Show the plots
         plt.tight_layout()
+        plt.savefig("plot.png")
         plt.show()
 
-    def run(self, instruction):
+    def run(self, list_instruction):
         """Generates multiple triggers for the given instruction."""
-        print(f"Generate triggers for instruction: {instruction}")
-        triggers = self._generate_triggers(instruction)
+        print(f"Generate triggers for instruction: {list_instruction}")
+        triggers = self._generate_triggers(list_instruction)
         self.plot_score_loss_n()
 
         return triggers
@@ -433,17 +420,22 @@ class TriggerValidator:
         self.p_value = config["p_value"]  # Statistical significance level.
         self.logging = pd.DataFrame()  # Logging dataframe.
 
-    def _eval_triggers(self, instruction: str, triggers: List[str]) -> torch.Tensor:
+    def _eval_triggers(self, list_instruction: List[str], triggers: List[str]) -> torch.Tensor:
 
-        instructions = [instruction for _ in triggers]
-        prompts = [t + instruction for t in triggers]
+        # Create a list of full prompts by appending each trigger to the instruction
+        instructions = random.choices(list_instruction, k=len(triggers))
+        prompts = [instructions[i] + t for i, t in enumerate(triggers)]    # Combine the instruction with each trigger
+
+        # Use the language model to generate responses for each prompt
         generations = self.model.generate(prompts, max_tokens=self.max_generations_tokens)
+
+        # Apply the scoring function to evaluate how well the responses meet the criteria defined by the scoring function
         score_array = self.scoring_function.score(instructions, generations, prompts)
 
         return score_array
-
+    
     def _add_logging(self, 
-                     instruction: str, 
+                     list_instruction: List[str], 
                      trigger: str, 
                      mean: float, 
                      std: float,
@@ -452,7 +444,7 @@ class TriggerValidator:
         logging_json = {'model_name': self.config["model"],
                         'scoring_method': self.config["scoring_type"],
                         'trigger': trigger,
-                        'instruction': instruction,
+                        'instruction': list_instruction,
                         'mean': mean,
                         'std': std,
                         'z': z,
@@ -465,39 +457,37 @@ class TriggerValidator:
         
         return self.logging 
     
-    def validate(self, instruction: str, triggers: List[str]) -> List[str]:
-        
-        triggers_validated: Set[str] = set()
-                    
-        for t in tqdm(triggers): 
-        
+    def validate(self, list_instruction: List[str], triggers: List[str]) -> List[str]:
+        triggers_with_z: List[Tuple[str, float]] = []  # List to store triggers and their z-scores
+
+        for t in tqdm(triggers):
             with torch.no_grad():
-                resampled_triggers = [t]*self.nb_samples
-                score_array = self._eval_triggers(instruction, resampled_triggers)
+                resampled_triggers = [t] * self.nb_samples
+                score_array = self._eval_triggers(list_instruction, resampled_triggers)
 
                 mean = score_array.mean().item()
                 std = score_array.std().item()
-                z = (mean - self.threshold)/(std/np.sqrt(self.nb_samples))
-                z_critical = stats.norm.ppf(1-self.p_value) 
+                z = (mean - self.threshold) / (std / np.sqrt(self.nb_samples))
+                z_critical = stats.norm.ppf(1 - self.p_value)
 
-            self._add_logging(instruction, 
-                              t, 
-                              mean, 
-                              std,
-                              z)
+                self._add_logging(list_instruction, t, mean, std, z)
 
-            if z>=z_critical:
-                triggers_validated.add(t)
+                # Store the trigger and z-score if it meets the criteria
+                if z >= z_critical:
+                    triggers_with_z.append((t, z))
 
-        return list(triggers_validated)
+        # Sort the triggers by their z-scores in descending order
+        sorted_triggers_with_z = sorted(triggers_with_z, key=lambda x: x[1], reverse=True)
 
+        # Extract the sorted triggers
+        sorted_triggers = [trigger for trigger, _ in sorted_triggers_with_z]
 
-    def run(self, instruction: str, triggers: List[str]) -> List[str]:
+        return sorted_triggers
 
-        print(f"Validate triggers for instruction: {instruction}")
+    def run(self, list_instruction: List[str], triggers: List[str]) -> List[str]:
 
-        triggers_validated = self.validate(instruction, triggers)
+        print(f"Validate triggers for instruction: {list_instruction}")
+
+        triggers_validated = self.validate(list_instruction, triggers)
 
         return triggers_validated
-
-
--- src/jailbreak.py
+++ src/jailbreak.py
@@ -85,31 +85,31 @@ class JailBreak:
         logging_generator = self._read_csv(self.logging_generator_path_csv)
         logging_validator = self._read_csv(self.logging_validator_path_csv)
     
-        for instruction in instructions: 
-            trigger_generator = TriggerGenerator(self.model, 
-                                                self.device, 
-                                                self.config,
-                                                self.reference_embedding, 
-                                                self.tokenizer_surrogate_model)
-            
-            trigger_validator = TriggerValidator(self.model, 
-                                                self.device, 
-                                                self.config)
-            
-            triggers[instruction] = trigger_generator.run(instruction)
+        # for instruction in instructions: 
+        self.trigger_generator = TriggerGenerator(self.model, 
+                                            self.device, 
+                                            self.config,
+                                            self.reference_embedding, 
+                                            self.tokenizer_surrogate_model)
+        
+        self.trigger_validator = TriggerValidator(self.model, 
+                                            self.device, 
+                                            self.config)
+        
+        triggers[instructions[0]] = self.trigger_generator.run(instructions)
 
-            logging_generator = pd.concat([logging_generator, trigger_generator.return_logging()], ignore_index=True)
-            logging_generator.to_json(self.logging_generator_path)
-            logging_generator.to_csv(self.logging_generator_path_csv, index=False)
-            with open(self.triggers_path, 'w') as f:
-                json.dump(triggers, f)
+        logging_generator = pd.concat([logging_generator, self.trigger_generator.return_logging()], ignore_index=True)
+        logging_generator.to_json(self.logging_generator_path)
+        logging_generator.to_csv(self.logging_generator_path_csv, index=False)
+        with open(self.triggers_path, 'w') as f:
+            json.dump(triggers, f)
 
-            triggers_validate[instruction] = trigger_validator.run(instruction, triggers[instruction])
+        triggers_validate[instructions[0]] = self.trigger_validator.run(instructions, triggers[instructions[0]])
 
-            logging_validator = pd.concat([logging_validator, trigger_validator.return_logging()], ignore_index=True)
-            logging_validator.to_json(self.logging_validator_path)
-            logging_validator.to_csv(self.logging_validator_path_csv, index=False)
-            with open(self.triggers_validate_path, 'w') as f:
-                json.dump(triggers_validate, f)
+        logging_validator = pd.concat([logging_validator, self.trigger_validator.return_logging()], ignore_index=True)
+        logging_validator.to_json(self.logging_validator_path)
+        logging_validator.to_csv(self.logging_validator_path_csv, index=False)
+        with open(self.triggers_validate_path, 'w') as f:
+            json.dump(triggers_validate, f)
 
         return  triggers, triggers_validate
