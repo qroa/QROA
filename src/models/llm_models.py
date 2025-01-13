5c6f95f6172e133c18b80789e51405057480e001
jawadhussein462 <jawadhussein462@gmail.com>
2025-01-13 10:34:24 +0100

    update

--- src/models/llm_models.py
+++ src/models/llm_models.py
@@ -220,6 +220,7 @@ class OpenaiModel(Model):
         "gpt-3.5-turbo-1106",
         "gpt-3.5-turbo-0125",
         "chatgpt-4o-latest",
+        "gpt-4o-2024-11-20",
         "gpt-4-0613"}
     
     def __init__(
