2b88a2363c445f15f772afbb927eb43010065c04
jawadhussein462 <jawadhussein462@gmail.com>
2025-01-18 19:50:55 +0100

    update

--- src/attack/qroa_models.py
+++ src/attack/qroa_models.py
@@ -36,22 +36,30 @@ class SurrogateModel(nn.Module):
         self.fc2 = nn.Linear(128, 32)
         self.fc3 = nn.Linear(32, 1)
 
+        from transformers import BertModel
+        self.backbone = BertModel.from_pretrained('bert-base-uncased')
+        self.fc4 = nn.Linear(768, 1)
+
         #self.fc1 = nn.Linear(self.emb_dim*self.len_coordinates, 128)
 
     def forward(self, x):
 
-        str_emb = self.emb[x]
+        x = self.backbone(x).last_hidden_state
+        x = x.mean(dim=1)
+        x = self.fc4(x)
 
-        # x = str_emb
-        x = str_emb.transpose(1, 2)
-        x = F.relu(self.conv1(x))
+        # str_emb = self.emb[x]
+
+        # # x = str_emb
+        # x = str_emb.transpose(1, 2)
+        # x = F.relu(self.conv1(x))
         
-        x = torch.flatten(x, start_dim=1)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc2(x)
-        x = F.relu(x)
-        x = self.fc3(x)
+        # x = torch.flatten(x, start_dim=1)
+        # x = self.fc1(x)
+        # x = F.relu(x)
+        # x = self.fc2(x)
+        # x = F.relu(x)
+        # x = self.fc3(x)
         return x
 
 
