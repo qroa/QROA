d77f6b840ea920af32ec496d5301bbaf7d13c8f7
jawadhussein462 <jawadhussein462@gmail.com>
2025-01-26 15:03:55 +0100

    update

--- src/global_constants.py
+++ src/global_constants.py
@@ -2,5 +2,5 @@ import os
 
 PERPLEXITY_MODEL_NAME = 'gpt2'
 PERPLEXITY_THRESHOLD = 1000
-MAX_PARALLELISM_LLM_MODELS = 100
+MAX_PARALLELISM_LLM_MODELS = 2000
 PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
