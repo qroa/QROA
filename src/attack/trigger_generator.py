7b67ff352c8ada9703ea251b177b3e00838851d2
jawadhussein462 <jawadhussein462@gmail.com>
2025-02-13 22:56:35 +0100

    update

--- src/attack/trigger_generator.py
+++ src/attack/trigger_generator.py
@@ -202,46 +202,46 @@ class TriggerGenerator:
         #     prompts
         # )
 
-        instructions = []
-        prompts = []
-        max_retries = 3
-        for instruction in list_instruction:
-            instructions += [instruction] * len(triggers)
-            prompts += [instruction + t for t in triggers]
-
-        for attempt in range(max_retries):
-
-            try:
-                generations = self.model.generate(
-                    prompts,
-                    max_tokens=self.max_generations_tokens
-                )
-                break
-            except Exception as e:
-                print(f"Attempt {attempt + 1} failed: {e}")
-                if attempt < max_retries - 1:
-                    time.sleep(2)  # Wait before retrying
-                else:
-                    print("Max retries reached. Returning None.")
-                    raise ValueError("Max retries reached. Returning None.")
-
-        scores = self.scoring_function.score(instructions, generations, prompts)
-        scores = [scores[i*len(triggers):(i+1)*len(triggers)] for i in range(len(list_instruction))]
-
-        # scores = []
-
+        # instructions = []
+        # prompts = []
+        # max_retries = 3
         # for instruction in list_instruction:
-        #     # Create a list of full prompts by appending each trigger to the instruction
-        #     instructions = [instruction] * len(triggers)
-        #     prompts = [instructions[i] + t for i, t in enumerate(triggers)]
+        #     instructions += [instruction] * len(triggers)
+        #     prompts += [instruction + t for t in triggers]
 
-        #     # Use the language model to generate responses for each prompt
-        #     generations = self.model.generate(prompts, max_tokens=self.max_generations_tokens)
+        # for attempt in range(max_retries):
 
-        #     # Apply the scoring function to evaluate how well the responses meet the criteria defined by the scoring function
-        #     score_instruction = self.scoring_function.score(instructions, generations, prompts)
+        #     try:
+        #         generations = self.model.generate(
+        #             prompts,
+        #             max_tokens=self.max_generations_tokens
+        #         )
+        #         break
+        #     except Exception as e:
+        #         print(f"Attempt {attempt + 1} failed: {e}")
+        #         if attempt < max_retries - 1:
+        #             time.sleep(2)  # Wait before retrying
+        #         else:
+        #             print("Max retries reached. Returning None.")
+        #             raise ValueError("Max retries reached. Returning None.")
 
-        #     scores.append(score_instruction)
+        # scores = self.scoring_function.score(instructions, generations, prompts)
+        # scores = [scores[i*len(triggers):(i+1)*len(triggers)] for i in range(len(list_instruction))]
+
+        scores = []
+
+        for instruction in list_instruction:
+            # Create a list of full prompts by appending each trigger to the instruction
+            instructions = [instruction] * len(triggers)
+            prompts = [instructions[i] + t for i, t in enumerate(triggers)]
+
+            # Use the language model to generate responses for each prompt
+            generations = self.model.generate(prompts, max_tokens=self.max_generations_tokens)
+
+            # Apply the scoring function to evaluate how well the responses meet the criteria defined by the scoring function
+            score_instruction = self.scoring_function.score(instructions, generations, prompts)
+
+            scores.append(score_instruction)
 
         score_array = torch.stack(scores).mean(dim=0)
 
